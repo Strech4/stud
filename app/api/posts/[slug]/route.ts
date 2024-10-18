889969917474063c4cecdc74aac5ac39e26d1ce7
Stretch4 <88911439+Strech4@users.noreply.github.com>
2024-10-18 19:42:44 +0200

    debug

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -1,8 +1,8 @@
 import { getPost } from "@/lib/post-helper";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+export const maxDuration = 25
 
-export const maxDuration = 20
 export const GET = async (
     request: NextRequest,
     props: { params: { slug: string } }
